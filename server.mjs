1c07cff10f6b8b3812604f1db3af4ff5da39c84c
Ibo <contact@iboibelings.nl>
2022-10-25 15:32:36 +0200

    test with broadcast

--- server.mjs
+++ server.mjs
@@ -1,23 +1,57 @@
-import { WebSocketServer } from 'ws';
-const PORT = process.env.PORT || 1122
+// //import { WebSocketServer } from 'ws';
+// import WebSocket, { WebSocketServer } from 'ws';
 
+
+// const wss = new WebSocketServer({ port: PORT });
+
+// console.log('server started')
+
+// wss.on('connection', function connection(ws) {
+//     ws.on('message', function message(data) {
+//       console.log('received: %s', data);
+//       ws.send(`I received the data ${data} from you`)
+//   //    wss.broadcoast(`hahaha lol ${data}`)
+//       console.log(wss.clients)
+//     });
+  
+//     ws.send('something');
+//   });
+
+
+
+
+
+
+import WebSocket, { WebSocketServer } from 'ws';
+const PORT = process.env.PORT || 1122
 const wss = new WebSocketServer({ port: PORT });
 
-console.log('server started')
+wss.on('connection', function connection(ws) {
+ws.on('message', function message(data, isBinary) {
+    wss.clients.forEach(function each(client) {
+    if (client.readyState === WebSocket.OPEN) {
+        client.send(data, { binary: isBinary });
+    }
+    });
+});
+});
+
+/*
 
 wss.on('connection', function connection(ws) {
-  ws.on('message', function message(data) {
-    console.log('received: %s', data);
-    ws.send(`I received the data ${data} from you`)
-//    wss.broadcoast(`hahaha lol ${data}`)
-    console.log(wss.clients)
+  ws.on('message', function message(data, isBinary) {
+    wss.clients.forEach(function each(client) {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(data, { binary: isBinary });
+      }
+    });
   });
-
-  ws.send('something');
 });
 
 
-/*
+
+
+
 wss.broadcast = function broadcast(msg) {
     console.log(msg);
     wss.clients.forEach(function each(client) {
