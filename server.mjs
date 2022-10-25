3c3ab052d22d068217bb8849d2473782c6e1fe93
Ibo <contact@iboibelings.nl>
2022-10-25 14:45:44 +0200

    maybe this owrks

--- server.mjs
+++ server.mjs
@@ -1,10 +1,9 @@
 import { WebSocketServer } from 'ws';
-
-
-
 const PORT = process.env.PORT || 1122
 
 const wss = new WebSocketServer({ port: PORT });
+
+console.log('server started')
 
 wss.on('connection', function connection(ws) {
   ws.on('message', function message(data) {
