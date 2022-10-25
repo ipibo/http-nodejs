2d0b5819876d970754299367d4d446a4301a8e62
Ibo <contact@iboibelings.nl>
2022-10-25 14:31:21 +0200

    small test

--- server.mjs
+++ server.mjs
@@ -3,6 +3,10 @@ import { WebSocketServer } from 'ws';
 
 console.log(process.env.PORT )
 
+const PORT = process.env.port || 1122
+
+console.log(PORT)
+
 const wss = new WebSocketServer({ port: 1122 });
 
 wss.on('connection', function connection(ws) {
