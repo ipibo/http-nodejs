26dce8275837e149415b062b3a29d828efa1a9fc
Ibo <contact@iboibelings.nl>
2022-10-25 15:14:26 +0200

    test

--- server.mjs
+++ server.mjs
@@ -9,18 +9,20 @@ wss.on('connection', function connection(ws) {
   ws.on('message', function message(data) {
     console.log('received: %s', data);
     ws.send(`I received the data ${data} from you`)
-    wss.broadcoast(`hahaha lol ${data}`)
+//    wss.broadcoast(`hahaha lol ${data}`)
+    console.log(wss.clients)
   });
 
   ws.send('something');
 });
 
 
+/*
 wss.broadcast = function broadcast(msg) {
     console.log(msg);
     wss.clients.forEach(function each(client) {
         client.send(msg);
     });
 };
-
+*/
 
